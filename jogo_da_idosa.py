ea280e74c98c69f7e878237bc66d6cb744c909bc
Diego1337 <Kayque7809828@gmail.com>
2025-02-25 19:50:41 -0300

    Criação Jogo

--- jogo_da_idosa.py
+++ jogo_da_idosa.py
@@ -1,3 +1,17 @@
 #PROJETO JOGO DA IDOSA
 
-print("Jogo da Idosa")
+tabuleiro = [
+            ["a","b","c"],
+            ["d","e","f"],
+            ["g","h","i"]
+            ]
+
+vez_jogador = "X"
+
+print(tabuleiro[0])
+print(tabuleiro[1])
+print(tabuleiro[2])
+
+posicao = input(f"{vez_jogador} Insira qual posição deseja preencher: ")
+
+print(posicao)
